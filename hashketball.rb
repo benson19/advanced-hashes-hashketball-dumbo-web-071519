03c3ac2f6abf540b16db49859213af0c66881aea
Matthew Guzman <tomaffew@me.com>
2019-07-09 06:37:23 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -134,3 +134,232 @@ def good_practices
   end
 end
 
+def num_points_scored(players_name)
+  game_hash.each do |location, team_data|
+    team_data.each do |attribute, data|
+      if attribute == :players 
+        data.each do |players|
+          players.each do |name, val|
+             #puts "name #{name}"
+             #puts "val #{val}"
+             if name == players_name
+              return val[:points] 
+             end
+          end
+        end
+      end
+    end
+  end
+end
+
+def shoe_size(players_name)
+  game_hash.each do |location, team_data|
+    team_data.each do |attribute, data|
+      if attribute == :players 
+        data.each do |players|
+          players.each do |name, val|
+             if name == players_name
+              return val[:shoe] 
+             end
+          end
+        end
+      end
+    end
+  end  
+end
+
+def team_colors(team_name)
+  game_hash.each do |location, team_data|
+    if team_data[:team_name] == team_name
+      return team_data[:colors]
+    end
+  end
+end
+
+def team_names
+  names = []
+  game_hash.each do |location, team_data|
+    team_data.each do |attribute, data|
+      if attribute == :team_name
+        names << data
+      end
+    end
+  end 
+  return names
+end
+
+def player_numbers(team_name)
+  numbers = []
+  game_hash.each do |location, team_data|
+    if team_data[:team_name] == team_name
+      team_data.each do |attribute, data|
+        if attribute == :players 
+          #puts attribute
+          data.each do |players|
+            players.each do |name, val|
+              #puts name
+               numbers << val[:number]
+            end
+          end
+        end
+      end
+    end
+  end 
+  return numbers  
+end
+
+def player_stats(players_name)
+  game_hash.each do |location, team_data|
+    team_data.each do |attribute, data|
+      if attribute == :players 
+        data.each do |players|
+          players.each do |name, val|
+             if name == players_name
+              return val 
+             end
+          end
+        end
+      end
+    end
+  end    
+end
+
+def big_shoe_rebounds
+  bigShoe = 0
+  rebounds = 0
+  game_hash.each do |location, team_data|
+    team_data.each do |attribute, data|
+      if attribute == :players 
+        data.each do |players|
+          players.each do |name, val|
+            if val[:shoe] > bigShoe
+              bigShoe = val[:shoe]
+              rebounds = val[:rebounds]
+             end
+          end
+        end
+      end
+    end
+  end    
+  return rebounds
+end  
+
+def most_points_scored
+  mostPoints = 0
+  mostVP = ""
+  game_hash.each do |location, team_data|
+    team_data.each do |attribute, data|
+      if attribute == :players 
+        data.each do |players|
+          players.each do |name, val|
+            if val[:points] > mostPoints
+              mostPoints = val[:points]
+              mostVP = name
+             end
+          end
+        end
+      end
+    end
+  end    
+  return mostVP
+end
+
+def all_stats
+  stats = []
+  homeStats = team_stats(:home)
+  awayStats = team_stats(:away)
+  stats = homeStats + awayStats
+  return stats
+end
+
+def team_stats(loc)
+  game_hash.each do |location, team_data|
+    if location == loc
+      team_data.each do |attribute, data|
+        if attribute == :players 
+          return data
+        end
+      end
+    end
+  end  
+end
+
+def winning_team
+  homeTeam = game_hash[:home][:team_name]
+  awayTeam = game_hash[:away][:team_name]
+  homeScore = 0
+  awayScore = 0
+
+  homeStats = team_stats(:home)
+  awayStats = team_stats(:away)
+
+  winner = ""
+  #puts "home #{homeStats}"
+  #puts "away #{awayStats}"
+
+  homeStats.each do |players|
+    players.each do |name, val|
+      homeScore += val[:points]
+    end
+  end
+
+  awayStats.each do |players|
+    players.each do |name, val|
+      awayScore += val[:points]
+    end
+  end
+
+  homeScore > awayScore ? winner = homeTeam : winner = awayTeam
+  return winner 
+end
+
+def array_of_players
+  names = []
+  game_hash.each do |location, team_data|
+    team_data.each do |attribute, data|
+      if attribute == :players 
+        #puts attribute
+        data.each do |players|
+          players.each do |name, val|
+            #puts name
+             names << name
+          end
+        end
+      end
+    end
+  end 
+  return names  
+end
+
+def player_with_longest_name
+  players = array_of_players.sort_by { 
+    |word| word.length 
+  }
+  return players[players.length - 1 ]
+end
+
+def most_steals 
+  maxSteals = 0
+  playersName = ""
+  all_stats.each do |players|
+    players.each do |name, val|
+      #puts val[:steals]
+      if val[:steals] > maxSteals
+        maxSteals = val[:steals]
+        playersName = name
+        #puts val[:steals]
+      else 
+        maxSteals = maxSteals
+      end
+    end
+  end
+  return playersName
+end
+
+def long_name_steals_a_ton?
+  name = player_with_longest_name
+  steals = most_steals
+  val = name == steals ? true : false
+  return val
+  
+end
